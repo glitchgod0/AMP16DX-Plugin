7cf91697c24a362b9d454b2628e662a1e1f6ccec
LysiX <smyler02@gmail.com>
2024-05-14 15:37:02 -0700

    debug rawfiles

--- source/main.c
+++ source/main.c
@@ -13,9 +13,9 @@
 #define attr_public __attribute__((visibility("default")))
 
 #define GOLDHEN_PATH "/data/GoldHEN"
-#define RB4DX_PATH GOLDHEN_PATH "/RB4DX"
-#define PLUGIN_CONFIG_PATH RB4DX_PATH "/RB4DX.ini"
-#define PLUGIN_NAME "RB4DX"
+#define RB4DX_PATH GOLDHEN_PATH "/RB4DBG"
+#define PLUGIN_CONFIG_PATH RB4DX_PATH "/RB4DBG.ini"
+#define PLUGIN_NAME "RB4DBG"
 #define final_printf(a, args...) klog("[" PLUGIN_NAME "] " a, ##args)
 
 #include <GoldHEN/Common.h>
@@ -25,7 +25,7 @@
 #include "ini.h"
 
 attr_public const char *g_pluginName = PLUGIN_NAME;
-attr_public const char *g_pluginDesc = "Plugin for loading Rock Band 4 Deluxe files, among other enhancements.";
+attr_public const char *g_pluginDesc = "Plugin for loading Rock Band 4 Debug files, among other enhancements.";
 attr_public const char *g_pluginAuth = "LysiX";
 attr_public uint32_t g_pluginVersion = 0x00000100; // 1.00
 
@@ -60,8 +60,8 @@ bool file_exists(const char* filename) {
 static struct proc_info procInfo;
 
 // ARKless file loading hook
-const char* RawfilesFolder = "/data/GoldHEN/RB4DX/";
-const char* GameRawfilesFolder = "data:/GoldHEN/RB4DX/";
+const char* RawfilesFolder = "/data/GoldHEN/RB4DBG/";
+const char* GameRawfilesFolder = "data:/GoldHEN/RB4DBG/";
 bool USTitleID = true;
 bool PrintRawfiles = true;
 bool PrintArkfiles = false;
@@ -96,91 +96,6 @@ void NewFile_hook(const char* path, FileMode mode) {
     return;
 }
 
-//speed hack
-typedef struct
-{
-    float speed;
-} SpeedConfiguration;
-bool ini_exists;
-static int SpeedHandler(void* user, const char* section, const char* name,
-                   const char* value)
-{
-    SpeedConfiguration* pconfig = (SpeedConfiguration*)user;
-
-    #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
-    if (MATCH("Settings", "SongSpeedMultiplier")) {
-        pconfig->speed = atof(value);
-    } else {
-        return 0;  /* unknown section/name, error */
-    }
-    return 1;
-}
-
-void(*GameRestart)(void*, bool);
-void(*SetMusicSpeed)(void*, float);
-
-HOOK_INIT(GameRestart);
-
-void GameRestart_hook(void* thisGame, bool restart) {
-    HOOK_CONTINUE(GameRestart, void (*)(void*, bool), thisGame, restart);
-    SpeedConfiguration SpeedConfig;
-    float speed = 1.00;
-
-    //read ini
-    if (ini_parse(PLUGIN_CONFIG_PATH, SpeedHandler, &SpeedConfig) < 0) {
-        final_printf("Can't load 'RB4DX.ini'\n");
-    }
-
-    if (SpeedConfig.speed == 0.00)
-        SpeedConfig.speed = 1.00;
-    if (ini_exists)
-        speed = SpeedConfig.speed;
-
-    if (speed > 0.00 && speed != 1.00){
-        SetMusicSpeed(thisGame, speed);
-        final_printf("Music speed: %.2f\n", speed);
-        DoNotification("Music Speed Set: %.2f", speed);
-    }
-    return;
-}
-
-//TODDO: force the readfile function to look for text format instead of binary
-
-//void(*datareadfile)(long, const char*, bool);
-//
-//hook_init(datareadfile);
-//
-//void datareadfile_hook(long l, const char* filename, bool b){
-//    final_printf("attempting to read dta %s\n", filename);
-//    if (strcmp(filename, "dx/ui/dx_init.dta") == 0) {
-//        final_printf("dx.dta read detected!\n");
-//        final_printf("long = %ld\n", l);
-//        final_printf("bool = %d\n", b);
-//        hook_continue(datareadfile, void(*)(long, const char*, bool), l, filename, 0);
-//        //if (b == 1)
-//        //    hook_continue(datareadfile, void(*)(const char*, bool), filename, 0);
-//        //else
-//        //    hook_continue(datareadfile, void(*)(const char*, bool), filename, 1);
-//    } else
-//        hook_continue(datareadfile, void(*)(long, const char*, bool), l, filename, b);
-//    return;
-//}
-
-//read lightbar status
-
-int(*TscePadSetLightBar)(int handle, OrbisPadColor *inputColor);
-
-HOOK_INIT(TscePadSetLightBar);
-
-void TscePadSetLightBar_hook(int handle, OrbisPadColor *inputColor) {
-    //final_printf("Set Light Bar Color:\n"); //disabled due to log spam
-    //final_printf("R: %d\n", inputColor->r);
-    //final_printf("G: %d\n", inputColor->g);
-    //final_printf("B: %d\n", inputColor->b);
-    scePadSetLightBar(handle, inputColor);
-    return;
-}
-
 #define ADDR_OFFSET 0x00400000
 int32_t attr_public module_start(size_t argc, const void *args)
 {
@@ -204,29 +119,18 @@ int32_t attr_public module_start(size_t argc, const void *args)
         return 0;
     }
     
-    if (strcmp(procInfo.version, "02.21") != 0) {
-        final_printf("This plugin is only compatible with version 02.21 of Rock Band 4.\n");
+    if (strcmp(procInfo.version, "01.00") != 0) {
+        final_printf("This plugin is only compatible with Debug version 01.00 of Rock Band 4.\n");
         return 0;
     }
 
-    ini_exists = file_exists(PLUGIN_CONFIG_PATH);
+    final_printf("Applying RB4DBG hooks...\n");
+    DoNotificationStatic("RB4DBG Plugin loaded!");
 
-    final_printf("Applying RB4DX hooks...\n");
-    DoNotificationStatic("RB4DX Plugin loaded!");
-
-    //configuration GameConfig; //TODO: implement game configuration
-
-    //DataReadFile = (void*)(procInfo.base_address + 0x002205e0);
-    NewFile = (void*)(procInfo.base_address + 0x00376d40);
-    GameRestart = (void*)(procInfo.base_address + 0x0a46710);
-    SetMusicSpeed = (void*)(procInfo.base_address + 0x00a470e0);
-    TscePadSetLightBar = (void*)(procInfo.base_address + 0x012450d0);
+    NewFile = (void*)(procInfo.base_address + 0x001e8990);
 
     // apply all hooks
-    HOOK(GameRestart);
     HOOK(NewFile);
-    HOOK(TscePadSetLightBar);
-    //HOOK(DataReadFile);
 
     return 0;
 }
@@ -235,9 +139,6 @@ int32_t attr_public module_stop(size_t argc, const void *args)
 {
     final_printf("Stopping plugin...\n");
     // unhook everything just in case
-    UNHOOK(GameRestart);
     UNHOOK(NewFile);
-    UNHOOK(TscePadSetLightBar);
-    //UNHOOK(DataReadFile);
     return 0;
 }
